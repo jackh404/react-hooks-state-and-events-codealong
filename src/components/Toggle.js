3847920cfc60d6d1946a6b5f4a29876b19f818bb
Jack Henza <jackhenza@gmail.com>
2023-10-26 09:10:13 -0400

    button!

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [power, switchPower] = useState(false)
+  return <button onClick={()=>switchPower((current)=>!current)}>{power? 'ON' : 'OFF'}</button>;
 }
 
 export default Toggle;
